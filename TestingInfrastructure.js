764c9a6f7387e49489e16d018d7874fe04007e93
RTurney <r.turney93@gmail.com>
2021-02-17 14:47:27 +0000

    Refactor tests. Add specHelper.js. All tests green

--- TestingInfrastructure.js
+++ TestingInfrastructure.js
@@ -1,31 +1,3 @@
-function it(label, callback) {
-  console.log(label);
-  callback();
-}
-
-function expect(expected) {
-  return {
-    toEqual(actual) {
-      if (expected === actual) {
-        console.log("Pass");
-      } else {
-        console.warn("Fail");
-      }
-    },
-  };
-}
-
-function pageHasContent(str) {
-  if (document.documentElement.textContent.includes(str)) {
-    console.log("page has content!");
-  } else {
-    console.warn("page doesn't have content!");
-  }
-}
-
-let page;
-page = document.documentElement;
-
 //practice test 1
 // it("should add 2 + 2 to make 4", function () {
 //   expect(2 + 2).toEqual(4);
@@ -45,12 +17,11 @@ it("should have content that says NotePad!", () => {
 //   document.getElementById(id).click(); // Click on the checkbox
 // }
 
-function fillInContentById(id, content) {
-  document.getElementById(id).value = content;
-}
 
 it("page should have content after filling in form and pressing button", () => {
+  clear();
   fillInContentById("addNoteField", "Note content blah blah blah");
   addNote();
   expect(noteList[0]).toEqual("Note content blah blah blah");
+  clear();
 });
---
+++ specHelper.js
@@ -0,0 +1,39 @@
+// Variables
+let page;
+page = document.documentElement;
+
+// Functions
+function it(label, callback) {
+  console.log(label);
+  callback();
+}
+
+function expect(expected) {
+  return {
+    toEqual(actual) {
+      if (expected === actual) {
+        console.log("Pass");
+      } else {
+        console.warn("Fail");
+      }
+    },
+  };
+}
+
+function pageHasContent(str) {
+  if (page.textContent.includes(str)) {
+    console.log("page has content!");
+  } else {
+    console.warn("page doesn't have content!");
+  }
+}
+
+function fillInContentById(id, content) {
+  document.getElementById(id).value = content;
+}
+
+
+function clear() {
+  noteList = [];
+  fillInContentById('addNoteField', '')
+}
