d9432586a0530baa48124a2885fc2a6620132605
RTurney <r.turney93@gmail.com>
2021-02-17 14:39:17 +0000

    Add addNote function. All tests green

--- TestingInfrastructure.js
+++ TestingInfrastructure.js
@@ -9,7 +9,7 @@ function expect(expected) {
       if (expected === actual) {
         console.log("Pass");
       } else {
-        console.log("Fail");
+        console.warn("Fail");
       }
     },
   };
@@ -19,36 +19,38 @@ function pageHasContent(str) {
   if (document.documentElement.textContent.includes(str)) {
     console.log("page has content!");
   } else {
-    console.log("page doesn't have content!");
+    console.warn("page doesn't have content!");
   }
 }
 
 let page;
 page = document.documentElement;
 
-it("should add 2 + 2 to make 4", function () {
-  expect(2 + 2).toEqual(4);
+//practice test 1
+// it("should add 2 + 2 to make 4", function () {
+//   expect(2 + 2).toEqual(4);
+// });
+
+// practice test 2
+// it("should add 2 + 3 to make 4", function () {
+//   expect(2 + 3).toEqual(4);
+// });
+
+it("should have content that says NotePad!", () => {
+  pageHasContent("NotePad");
 });
 
-it("should add 2 + 3 to make 4", function () {
-  expect(2 + 3).toEqual(4);
-});
-
-it("should have content that says pee block!", () => {
-  pageHasContent("Pee block!!");
-});
-
-function clickButtonById(id) {
-  document.getElementById(id).click(); // Click on the checkbox
-}
+// Commented out for now
+// function clickButtonById(id) {
+//   document.getElementById(id).click(); // Click on the checkbox
+// }
 
 function fillInContentById(id, content) {
   document.getElementById(id).value = content;
 }
 
 it("page should have content after filling in form and pressing button", () => {
-  fillInContentById("addNote", "Note title");
   fillInContentById("addNoteField", "Note content blah blah blah");
-  clickButtonById("addNoteButton");
-  pageHasContent("Note content blah blah blah");
+  addNote();
+  expect(noteList[0]).toEqual("Note content blah blah blah");
 });
